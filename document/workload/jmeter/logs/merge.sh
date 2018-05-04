65df5796342085bdee73ee06838f27942afb6991
Unknown <daumayr162@gmail.com>
2018-05-04 09:50:34 +0200

    update merge script

--- document/workload/jmeter/logs/merge.sh
+++ document/workload/jmeter/logs/merge.sh
@@ -9,11 +9,11 @@ do
   tail -n +2  temp.data >>  acme.data # Append from the 2nd line each file
 done
 
-for i in $(seq 1 6)
+for i in $(seq 1 10)
 do
   F="AcmeAir_UT$i.jtl"
   awk -v i="$i" -v vm="Untraced" 'NR==1{print $0 ",Iteration,VM"} NR>1{print $0 "," i "," vm}' $F > temp.data
   tail -n +2  temp.data >>  acme.data # Append from the 2nd line each file
 done
 
-tar -cvjSf acme.data.tar.bz2 acme.data
+bzip2 acme.data
